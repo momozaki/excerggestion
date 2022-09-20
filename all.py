d5cf9b995c6b7e6ced56e949078634c4e62ce12c
keitosuwahara <108104699+keitosuwahara@users.noreply.github.com>
2022-09-20 10:49:21 +0900

    a

--- all.py
+++ all.py
@@ -1,7 +1,14 @@
+import os
+
+os.makedirs("./all_edited_file",exist_ok=True)
+os.makedirs("./edited_rota",exist_ok=True)
+os.makedirs("./edited_slide",exist_ok=True)
+os.makedirs("./edited_flip",exist_ok=True)
+
+
 from rota import Rota
 from slide import Slide
 from flip import Flip
-
 
 
 Rota()
